eb4619aff1eb0da8ecbd3fed9e5c60d13fdc8fbc
Yofenry <72547311+Yofenry@users.noreply.github.com>
2021-10-10 20:37:14 +1000

    The 3 Files That Are Required

---
+++ Yofservices.py
@@ -0,0 +1,20 @@
+import webbrowser
+var1 = input("What App Would You Like TO Open(For List Of MiniApps Type help without capitals And Press Enter) ")
+if var1 == "help":
+   var1 = webbrowser.open('https://pastebin.com/DvCNLvJy')
+   var1 = input("Type Another Command ")
+   
+if var1 == "Help":
+   var1 = input("You Dont Type Help With A Capital Theres No Capitals In Any MiniApp(Yes help Is A MiniApp) ")
+if var1 == "yoftube":
+   exec(open('yoftube.py').read())
+   var1 = input("Now Type The Name Of Another MiniApp ")
+
+if var1 == "armymod":
+   exec(open('armymod.py').read())
+   var1 = input("Now Type The Name Of Another MiniApp ")
+
+if var1 == "yodafenry":
+   webbrowser.open('https://ibb.co/Zh48QW0')
+   var1 = input("Yofenry I Am ")
+
---
+++ armymod.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open('https://www.planetminecraft.com/mod/army-mod-5317877/')
